56ca97fa72e8c28374222841a4344bcc7a136807
Joshua <joshuaagbeluyi20@gmail.com>
2025-05-13 08:48:05 +0100

    Updated app

--- authproject/authapp/urls.py
+++ authproject/authapp/urls.py
@@ -5,10 +5,10 @@ from . import views
 urlpatterns = [
     #path('authapp/signup/', views.signup, name='signup'),
     # path('authapp/signup/done/', views.signupDone, name='signup_done'),
-    path('authapp/login/', views.login, name='login'),
+    path('login/', views.login, name='login'),
     #path('authapp/logout/', views.logout, name='logout'),  
-    path('authapp/password_change/', views.passwordChange, name='password_change'),  
-    path('authapp/password_change/done/', views.passwordChangeDone, name='password_change_done'),
+    path('password_change/', auth_views.PasswordChangeView.as_view(), name='password_change'),  
+    path('password_change/done/', auth_views.PasswordChangeDoneView.as_view, name='password_change_done'),
     #path('authapp/password_reset/', views.passwordReset, name='password_reset'),
     #path('authapp/password_reset/done/', views.passwordResetDone, name='password_reset_done'),
     #path('authapp/reset/<uidb64>/<token>/', views.passwordResetConfirm, name='password_reset_confirm'),
